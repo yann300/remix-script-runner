8e99b3774029a4eee82d1c8668793410a89d7c04
yann300 <yann.levreau@gmail.com>
2020-02-11 10:34:01 +0100

    initial commit

---
+++ src/script-runner.js
@@ -0,0 +1,57 @@
+'use strict'
+import { connectIframe, PluginClient } from '@remixproject/plugin'
+import ethers from 'ethers' // eslint-disable-line
+import Web3 from 'web3'
+import swarmgw_fn from 'swarmgw'
+window.swarmgw = swarmgw_fn()
+window.ethers = ethers
+window.Web3 = Web3
+
+class CodeExecutor extends PluginClient {
+  execute (script) {
+    if (script) {
+      try {
+        (new Function(script))()
+      } catch (e) {
+        this.emit('error', {
+          data: [e.message]
+        })
+      }
+    }
+  }
+}
+window.remix = new CodeExecutor()
+connectIframe(window.remix)
+
+window.web3Provider = {
+  sendAsync(payload, callback) {
+    window.remix.call('appWeb3Provider', 'sendAsync', payload)
+      .then(result => callback(null, result))
+      .catch(e => callback(e))
+  }
+}
+window.web3 = new Web3(window.web3Provider)
+
+console.log = function () {
+  window.remix.emit('log', {
+    data: Array.from(arguments)
+  })
+}
+
+console.info = function () {
+  window.remix.emit('info', {
+    data: Array.from(arguments)
+  })
+}
+
+console.warn = function () {
+  window.remix.emit('warn', {
+    data: Array.from(arguments)
+  })
+}
+
+console.error = function () {
+  window.remix.emit('error', {
+    data: Array.from(arguments)
+  })
+}
