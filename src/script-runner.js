e73b9aab3bc54399f99d24119873b7e8d012a9f9
Aniket-Engg <aniketengg.225@gmail.com>
2022-02-03 21:48:16 +0530

    mocha

--- src/script-runner.js
+++ src/script-runner.js
@@ -6,16 +6,32 @@ import Web3 from 'web3'
 import swarmgw_fn from 'swarmgw'
 import * as starknet from 'starknet'
 var chai = require('chai')
+var browserfs = require('browserfs')
 window.swarmgw = swarmgw_fn()
 window.ethers = ethers
 window.Web3 = Web3
+
+window.onload = () => {
+  browserfs.install(window)
+  browserfs.configure({
+    fs: "LocalStorage"
+  }, function(e) {
+    console.log('browser fs configured for rsr')
+  })
+}
 
 class CodeExecutor extends PluginClient {
   execute (script) {
     if (script) {
       try {
-        (new Function(script))()
+        // (new Function(script))()
+       const mocha = new Mocha()
+       mocha.addFile('default_workspace/scripts/deploy_web3.js')
+       console.log('file added, running tests----->')
+       const resss = mocha.run()
+       console.log('ress----->', resss)
       } catch (e) {
+        console.log('error in execute----->', e)
         this.emit('error', {
           data: [e.message]
         })
@@ -23,6 +39,7 @@ class CodeExecutor extends PluginClient {
     }
   }
 }
+
 window.remix = new CodeExecutor()
 createClient(window.remix)
 
@@ -39,11 +56,11 @@ window.starknet = starknet
 window.chai = chai
 
 
-console.log = function () {
-  window.remix.emit('log', {
-    data: Array.from(arguments)
-  })
-}
+// console.log = function () {
+//   window.remix.emit('log', {
+//     data: Array.from(arguments)
+//   })
+// }
 
 console.info = function () {
   window.remix.emit('info', {
