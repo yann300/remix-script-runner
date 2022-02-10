381b4c038f914eeaf6257641f566aead7cb51839
Aniket-Engg <aniketengg.225@gmail.com>
2022-02-10 16:24:27 +0530

    constants

--- src/script-runner.js
+++ src/script-runner.js
@@ -5,83 +5,20 @@ import { ethers } from 'ethers' // eslint-disable-line
 import Web3 from 'web3'
 import swarmgw_fn from 'swarmgw'
 import * as starknet from 'starknet'
-var chai = require('chai')
-var mocha = require('mocha')
+const chai = require('chai')
+const mocha = require('mocha')
+const {
+  EVENT_RUN_BEGIN,
+  EVENT_RUN_END,
+  EVENT_TEST_FAIL,
+  EVENT_TEST_PASS,
+  EVENT_SUITE_BEGIN,
+  EVENT_SUITE_END
+} = mocha._runnerClass.constants
+
 mocha.setup('bdd')
 mocha.checkLeaks()
 mocha.cleanReferencesAfterRun(false)
-
-const mochaConstant = {
-  /**
-   * Emitted when {@link Hook} execution begins
-   */
-  EVENT_HOOK_BEGIN: 'hook',
-  /**
-   * Emitted when {@link Hook} execution ends
-   */
-  EVENT_HOOK_END: 'hook end',
-  /**
-   * Emitted when Root {@link Suite} execution begins (all files have been parsed and hooks/tests are ready for execution)
-   */
-  EVENT_RUN_BEGIN: 'start',
-  /**
-   * Emitted when Root {@link Suite} execution has been delayed via `delay` option
-   */
-  EVENT_DELAY_BEGIN: 'waiting',
-  /**
-   * Emitted when delayed Root {@link Suite} execution is triggered by user via `global.run()`
-   */
-  EVENT_DELAY_END: 'ready',
-  /**
-   * Emitted when Root {@link Suite} execution ends
-   */
-  EVENT_RUN_END: 'end',
-  /**
-   * Emitted when {@link Suite} execution begins
-   */
-  EVENT_SUITE_BEGIN: 'suite',
-  /**
-   * Emitted when {@link Suite} execution ends
-   */
-  EVENT_SUITE_END: 'suite end',
-  /**
-   * Emitted when {@link Test} execution begins
-   */
-  EVENT_TEST_BEGIN: 'test',
-  /**
-   * Emitted when {@link Test} execution ends
-   */
-  EVENT_TEST_END: 'test end',
-  /**
-   * Emitted when {@link Test} execution fails
-   */
-  EVENT_TEST_FAIL: 'fail',
-  /**
-   * Emitted when {@link Test} execution succeeds
-   */
-  EVENT_TEST_PASS: 'pass',
-  /**
-   * Emitted when {@link Test} becomes pending
-   */
-  EVENT_TEST_PENDING: 'pending',
-  /**
-   * Emitted when {@link Test} execution has failed, but will retry
-   */
-  EVENT_TEST_RETRY: 'retry',
-  /**
-     * Initial state of Runner
-     */
-   STATE_IDLE: 'idle',
-   /**
-    * State set to this value when the Runner has started running
-    */
-   STATE_RUNNING: 'running',
-   /**
-    * State set to this value when the Runner has stopped
-    */
-   STATE_STOPPED: 'stopped'
- }
-
 
 // this reporter outputs test results, indenting two spaces per suite
 class MochaReporter {
@@ -90,30 +27,30 @@ class MochaReporter {
     const stats = runner.stats
 
     runner
-      .once(mochaConstant.EVENT_RUN_BEGIN, () => {
+      .once(EVENT_RUN_BEGIN, () => {
         console.log('Running tests....')
       })
-      .on(mochaConstant.EVENT_SUITE_BEGIN, (suite) => {
+      .on(EVENT_SUITE_BEGIN, (suite) => {
         if(suite.title) {
           this.increaseIndent()
           console.log(`${suite.title}`)
         }
       })
-      .on(mochaConstant.EVENT_SUITE_END, (suite) => {
+      .on(EVENT_SUITE_END, (suite) => {
         this.decreaseIndent()
         if(suite.root) suite.suites = []
       })
-      .on(mochaConstant.EVENT_TEST_PASS, test => {
+      .on(EVENT_TEST_PASS, test => {
         console.info(`${this.indent()} ✓ ${test.title} (${test.duration} ms)`)
       })
-      .on(mochaConstant.EVENT_TEST_FAIL, (test, err) => {
+      .on(EVENT_TEST_FAIL, (test, err) => {
         console.error(`${this.indent()} ✘ ${test.title} (${test.duration} ms)`)
         console.error(`${this.indent()} ${this.indent()} Expected: ${err.expected}`)
         console.error(`${this.indent()} ${this.indent()} Actual: ${err.actual}`)
         console.error(`${this.indent()} ${this.indent()} Message: ${err.message}`)
 
       })
-      .once(mochaConstant.EVENT_RUN_END, () => {
+      .once(EVENT_RUN_END, () => {
         console.log(`${stats.passes} passing & ${stats.failures} failing (${stats.duration} ms)`)
       })
   }
@@ -143,7 +80,6 @@ class CodeExecutor extends PluginClient {
       try {
         (new Function(script))()
       } catch (e) {
-        console.log('error in execute----->', e)
         this.emit('error', {
           data: [e.message]
         })
