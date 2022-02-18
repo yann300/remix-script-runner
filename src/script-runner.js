254cf6f67d58d410545696e68f1153fef5f34e6c
Aniket <30843294+Aniket-Engg@users.noreply.github.com>
2022-02-18 17:32:46 +0530

    Revert "access bytecode"

--- src/script-runner.js
+++ src/script-runner.js
@@ -96,7 +96,7 @@ window.ethers.getContractFactory = (contractName, signer = null) => {
   return new Promise((resolve, reject) => {
     window.remix.call('compilerArtefacts', 'getArtefactsByContractName', contractName)
     .then((result) => {
-      resolve(new ethers.ContractFactory(result.abi, result.bytecode, signer || (new ethers.providers.Web3Provider(web3Provider)).getSigner()))
+      resolve(new ethers.ContractFactory(result.abi, result.evm.bytecode.object, signer || (new ethers.providers.Web3Provider(web3Provider)).getSigner()))
     })
     .catch(e => reject(e))
   })
