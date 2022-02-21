1eb84cd1e9fe82aae6c539a13b563a2bc14dd770
Aniket-Engg <aniketengg.225@gmail.com>
2022-02-21 14:36:22 +0530

    getContractFactory

--- src/hhEthers.js
+++ src/hhEthers.js
@@ -1,9 +1,13 @@
-export const getContractFactory = (contractName, signer = null) => {
-    return new Promise((resolve, reject) => {
-      window.remix.call('compilerArtefacts', 'getArtefactsByContractName', contractName)
+export const getContractFactory = (contractNameOrABI, bytecode=null, signer = null) => {
+  return new Promise((resolve, reject) => {
+    if(typeof contractNameOrABI === 'string') {
+      window.remix.call('compilerArtefacts', 'getArtefactsByContractName', contractNameOrABI)
       .then((result) => {
         resolve(new ethers.ContractFactory(result.abi, result.evm.bytecode.object, signer || (new ethers.providers.Web3Provider(web3Provider)).getSigner()))
       })
       .catch(e => reject(e))
-    })
-  }
+    } else {
+      resolve(new ethers.ContractFactory(contractNameOrABI, bytecode, signer || (new ethers.providers.Web3Provider(web3Provider)).getSigner()))
+    }
+  })
+}
