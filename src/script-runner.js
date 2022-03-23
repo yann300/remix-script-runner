def2d15e169ab7b1ee7daa77eb80d9b4b1e1478b
yann300 <yann.levreau@gmail.com>
2022-03-23 12:59:23 +0100

    add typescript

--- src/script-runner.js
+++ src/script-runner.js
@@ -1,5 +1,6 @@
 'use strict'
 import "@babel/polyfill"
+import * as ts from "typescript";
 import { createClient } from '@remixproject/plugin-iframe'
 import { PluginClient } from '@remixproject/plugin'
 import { ethers } from 'ethers' // eslint-disable-line
@@ -16,23 +17,71 @@ window.starknet = starknet
 window.chai = chai
 window.ethers = ethers
 
+const scriptReturns = {} // keep track of modules exported values
 window.require = (module) => {
-  if(window[module]) return window[module]
+  if (window[module]) return window[module] // library
+  if (window.__execPath__ && scriptReturns[window.__execPath__]) return scriptReturns[window.__execPath__][module] // module exported values
   else throw new Error(`${module} module require is not supported by Remix IDE`)
 }
 
 class CodeExecutor extends PluginClient {
-  execute (script) {
+  async execute (script, filePath, fromPath) {
+    filePath = filePath || 'scripts/script.ts'
+    const paths = filePath.split('/')
+    paths.pop()
+    fromPath = fromPath || paths.join('/') // get current execcution context path
     if (script) {
       try {
-        (new Function(script))()
+        script = ts.transpileModule(script, { moduleName: filePath, filePath,
+        compilerOptions: {
+         target: ts.ScriptTarget.ES2015,
+         module: ts.ModuleKind.CommonJS 
+        }});
+        script = script.outputText;
+
+        // extract all the "require", execute them and store the returned values.
+        const regexp = /require\("(.*?)"\)/g
+        const array = [...script.matchAll(regexp)];
+
+        for (const regex of array) {
+          let file = regex[1]
+          let absolutePath = file
+          if (file.startsWith('./')) {
+            absolutePath = paths.join('/') + file.substring(1)
+          }
+          if (!scriptReturns[fromPath]) scriptReturns[fromPath] = {}
+          scriptReturns[fromPath][file] = await this.executeFile(absolutePath, fromPath)
+        }
+
+        // execute the script
+        script = `const exports = {};
+                  window.__execPath__ = "${fromPath}"
+                  ${script}; 
+                  return exports`
+        const returns = (new Function(script))()
         if (mocha.suite && ((mocha.suite.suites && mocha.suite.suites.length) || (mocha.suite.tests && mocha.suite.tests.length))) mocha.run()
+        return returns
       } catch (e) {
         this.emit('error', {
           data: [e.message]
         })
       }
     }
+  }
+
+  async _resolveFile (fileName) {
+    if (await this.call('fileManager', 'exists', fileName)) return await this.call('fileManager', 'readFile', fileName)
+    if (await this.call('fileManager', 'exists', fileName + '.js')) return await this.call('fileManager', 'readFile', fileName + '.js')
+    if (await this.call('fileManager', 'exists', fileName + '.ts')) return await this.call('fileManager', 'readFile', fileName + '.ts')
+  }
+
+  async executeFile (fileName, fromPath) {
+    try {
+      if (require(fileName)) return require(fileName)
+    } catch (e) {}
+    const content = await this._resolveFile(fileName)
+    const returns = await this.execute(content, fileName, fromPath)
+    return returns
   }
 }
 
